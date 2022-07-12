d8d1970d7f5356a9bcb754252a6cc8edf16485f3
soy hok <soyhok2018@gmail.com>
2022-07-12 17:28:13 +0700

    Add project files.

---
+++ LabExercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExercise1
+{
+    class Car
+    {
+        public string strModel;
+        public string strBrand;
+        public float fltPrice;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar1;
+            myCar1 = new Car();
+
+            Console.Write("What is your model of your car: ");
+            myCar1.strModel = Console.ReadLine();
+            Console.Write("What is your bramd of your car: ");
+            myCar1.strBrand = Console.ReadLine();
+            Console.Write("What is your price of your car: ");
+            myCar1.fltPrice = float.Parse(Console.ReadLine());
+            Console.WriteLine("Car Model is {0},brand is {1}, price is {2}", myCar1.strModel, myCar1.strBrand, myCar1.fltPrice);
+        }
+    }
+}
